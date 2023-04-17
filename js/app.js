285ebe873acea4febf6605216ff01b65470d438b
jaanosa11 <58842690+jaanosa11@users.noreply.github.com>
2023-04-17 19:04:11 +0800

    Pricing - revert to original

--- js/app.js
+++ js/app.js
@@ -45,184 +45,184 @@ home.addEventListener('click',function(){
 // });
 
 // Fetch json files and append to respective tables
-// async function fetchJSON() {
-//     const [colorResponse, bwResponse, prResponse] = await Promise.all([
-//         fetch('./json/full_color.json'),
-//         fetch('./json/black_white.json'),
-//         fetch('./json/pr.json')
-//     ]);
-//     const full_color = await colorResponse.json();
-//     const black_white = await bwResponse.json();
-//     const pr = await prResponse.json();
-//     return [full_color, black_white, pr];
-// }
+async function fetchJSON() {
+    const [colorResponse, bwResponse, prResponse] = await Promise.all([
+        fetch('./json/full_color.json'),
+        fetch('./json/black_white.json'),
+        fetch('./json/pr.json')
+    ]);
+    const full_color = await colorResponse.json();
+    const black_white = await bwResponse.json();
+    const pr = await prResponse.json();
+    return [full_color, black_white, pr];
+}
 
-// fetchJSON().then(([full_color, black_white, pr]) => {
-//     if (full_color) {
-//         var full_color_str = '';
-//         full_color.forEach(item => {
-//             full_color_str =
-//             `<div class="card-item p-2">
-//                 <div class="card-item-wrapper bc-2">
-//                     <div class="card-header p-3 bg-2 text-center text-white">
-//                         <span class="fw-bold">${item.Type}</span>
-//                         <h2>${item.Price}</h2>
-//                     </div>
-//                     <div class="card-item-body p-3">
-//                         <ul class="pb-3">`;
-//                         if (item.Values) {
-//                             var item_values = item.Values;
-//                             item_values.forEach(value => {
-//                                 if (value.Value) {
-//                                     if (value.Value === 'Add-On') {
-//                                         full_color_str +=
-//                                         `<li class="add-on"> <i class="bi bi-plus-square-fill"></i>
-//                                             ${value.Inclusions}
-//                                         </li>`;
-//                                     }else if(value.Value === 'No'){
-//                                         full_color_str +=
-//                                         `<li> <i class="bi bi-x-square-fill"></i>
-//                                             ${value.Inclusions}
-//                                         </li>`;
-//                                     }else{
-//                                         full_color_str +=
-//                                         `<li> <i class="bi bi-check-square"></i>
-//                                             ${value.Inclusions}
-//                                         </li>`;
-//                                     }
-//                                 }
-//                             });
-//                         }
-//             full_color_str += 
-//                         `</ul>
-//                         <button class="btn rounded-0 text-white bg-2 w-100 p-2 fw-bold text-uppercase" data-bs-toggle="modal" data-bs-target="#exampleModal">
-//                             Get Started
-//                         </button>
-//                     </div>
-//                 </div>
-//             </div>`;
+fetchJSON().then(([full_color, black_white, pr]) => {
+    if (full_color) {
+        var full_color_str = '';
+        full_color.forEach(item => {
+            full_color_str =
+            `<div class="card-item p-2">
+                <div class="card-item-wrapper bc-2">
+                    <div class="card-header p-3 bg-2 text-center text-white">
+                        <span class="fw-bold">${item.Type}</span>
+                        <h2>${item.Price}</h2>
+                    </div>
+                    <div class="card-item-body p-3">
+                        <ul class="pb-3">`;
+                        if (item.Values) {
+                            var item_values = item.Values;
+                            item_values.forEach(value => {
+                                if (value.Value) {
+                                    if (value.Value === 'Add-On') {
+                                        full_color_str +=
+                                        `<li class="add-on"> <i class="bi bi-plus-square-fill"></i>
+                                            ${value.Inclusions}
+                                        </li>`;
+                                    }else if(value.Value === 'No'){
+                                        full_color_str +=
+                                        `<li> <i class="bi bi-x-square-fill"></i>
+                                            ${value.Inclusions}
+                                        </li>`;
+                                    }else{
+                                        full_color_str +=
+                                        `<li> <i class="bi bi-check-square"></i>
+                                            ${value.Inclusions}
+                                        </li>`;
+                                    }
+                                }
+                            });
+                        }
+            full_color_str += 
+                        `</ul>
+                        <button class="btn rounded-0 text-white bg-2 w-100 p-2 fw-bold text-uppercase" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                            Get Started
+                        </button>
+                    </div>
+                </div>
+            </div>`;
 
-//             document.getElementById("table_full_color").innerHTML += full_color_str;
-//         });
-//     }
+            document.getElementById("table_full_color").innerHTML += full_color_str;
+        });
+    }
 
-//     if (black_white) {
-//         var bw_str = '';
-//         black_white.forEach(item => {
-//             bw_str =
-//             `<div class="card-item p-2">
-//                 <div class="card-item-wrapper bc-2">
-//                     <div class="card-header p-3 bg-2 text-center text-white">
-//                         <span class="fw-bold">${item.Type}</span>
-//                         <h2>${item.Price}</h2>
-//                     </div>
-//                     <div class="card-item-body p-3">
-//                         <ul class="pb-3">`;
-//                         if (item.Values) {
-//                             var item_values = item.Values;
-//                             item_values.forEach(value => {
-//                                 if (value.Value) {
-//                                     if (value.Value === 'Add-On') {
-//                                         bw_str +=
-//                                         `<li class="add-on"> <i class="bi bi-plus-square-fill"></i>
-//                                             ${value.Inclusions}
-//                                         </li>`;
-//                                     }else if(value.Value === 'No'){
-//                                         bw_str +=
-//                                         `<li> <i class="bi bi-x-square-fill"></i>
-//                                             ${value.Inclusions}
-//                                         </li>`;
-//                                     }else{
-//                                         bw_str +=
-//                                         `<li> <i class="bi bi-check-square"></i>
-//                                             ${value.Inclusions}
-//                                         </li>`;
-//                                     }
-//                                 }
-//                             });
-//                         }
-//                         bw_str += 
-//                         `</ul>
-//                         <button class="btn rounded-0 text-white bg-2 w-100 p-2 fw-bold text-uppercase" data-bs-toggle="modal" data-bs-target="#exampleModal">
-//                             Get Started
-//                         </button>
-//                     </div>
-//                 </div>
-//             </div>`;
+    if (black_white) {
+        var bw_str = '';
+        black_white.forEach(item => {
+            bw_str =
+            `<div class="card-item p-2">
+                <div class="card-item-wrapper bc-2">
+                    <div class="card-header p-3 bg-2 text-center text-white">
+                        <span class="fw-bold">${item.Type}</span>
+                        <h2>${item.Price}</h2>
+                    </div>
+                    <div class="card-item-body p-3">
+                        <ul class="pb-3">`;
+                        if (item.Values) {
+                            var item_values = item.Values;
+                            item_values.forEach(value => {
+                                if (value.Value) {
+                                    if (value.Value === 'Add-On') {
+                                        bw_str +=
+                                        `<li class="add-on"> <i class="bi bi-plus-square-fill"></i>
+                                            ${value.Inclusions}
+                                        </li>`;
+                                    }else if(value.Value === 'No'){
+                                        bw_str +=
+                                        `<li> <i class="bi bi-x-square-fill"></i>
+                                            ${value.Inclusions}
+                                        </li>`;
+                                    }else{
+                                        bw_str +=
+                                        `<li> <i class="bi bi-check-square"></i>
+                                            ${value.Inclusions}
+                                        </li>`;
+                                    }
+                                }
+                            });
+                        }
+                        bw_str += 
+                        `</ul>
+                        <button class="btn rounded-0 text-white bg-2 w-100 p-2 fw-bold text-uppercase" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                            Get Started
+                        </button>
+                    </div>
+                </div>
+            </div>`;
 
-//             document.getElementById("table_black_white").innerHTML += bw_str;
-//         });
-//     }
+            document.getElementById("table_black_white").innerHTML += bw_str;
+        });
+    }
 
-//     if (pr) {
-//         var pr_str = '';
-//         pr.forEach(item => {
-//             pr_str =
-//             `<div class="card-item p-2">
-//                 <div class="card-item-wrapper bc-1">
-//                     <div class="card-header p-3 bg-1 text-center text-white">
-//                         <span class="fw-bold">${item.Type}</span>
-//                         <h2>${item.Price}</h2>
-//                         <p>${item.Month}</p>
-//                     </div>
-//                     <div class="card-item-body p-3">`;
-//                         if (item.Services) {
-//                             var item_services = item.Services;
-//                             item_services.forEach(services => {
-//                                 pr_str +=
-//                                 `   <h5 class="text-center">
-//                                         ${services.Service}
-//                                     </h5>
-//                                 `;
-//                                 if (services.Values) {
-//                                     pr_str += `<ul class="pb-3">`
-//                                     var service = services.Values
-//                                     service.forEach(value => {
-//                                         if (value) {
+    if (pr) {
+        var pr_str = '';
+        pr.forEach(item => {
+            pr_str =
+            `<div class="card-item p-2">
+                <div class="card-item-wrapper bc-1">
+                    <div class="card-header p-3 bg-1 text-center text-white">
+                        <span class="fw-bold">${item.Type}</span>
+                        <h2>${item.Price}</h2>
+                        <p>${item.Month}</p>
+                    </div>
+                    <div class="card-item-body p-3">`;
+                        if (item.Services) {
+                            var item_services = item.Services;
+                            item_services.forEach(services => {
+                                pr_str +=
+                                `   <h5 class="text-center">
+                                        ${services.Service}
+                                    </h5>
+                                `;
+                                if (services.Values) {
+                                    pr_str += `<ul class="pb-3">`
+                                    var service = services.Values
+                                    service.forEach(value => {
+                                        if (value) {
                                           
-//                                             var inclusion = value.Inclusion ? value.Inclusion : '';
-//                                             var inc_value = value.Value ? value.Value : '';
-//                                             var included  = value.Included ? value.Included === 'No' ? '' : value.Included : '';
+                                            var inclusion = value.Inclusion ? value.Inclusion : '';
+                                            var inc_value = value.Value ? value.Value : '';
+                                            var included  = value.Included ? value.Included === 'No' ? '' : value.Included : '';
 
-//                                             if (inclusion.includes('Add on')) {
-//                                                 pr_str +=
-//                                                 `<li class="add-on"> <i class="bi bi-plus-square-fill"></i>
-//                                                     ${inc_value} ${inclusion} 
-//                                                 </li>`;
-//                                             }else if(included === ''){
-//                                                 pr_str +=
-//                                                 `<li> <i class="bi bi-x-square-fill"></i>
-//                                                     ${inc_value} ${inclusion}
-//                                                 </li>`;
-//                                             }else{
-//                                                 pr_str +=
-//                                                 `<li> <i class="bi bi-check-square"></i>
-//                                                     ${inc_value} ${inclusion}
-//                                                 </li>`;
-//                                             }
+                                            if (inclusion.includes('Add on')) {
+                                                pr_str +=
+                                                `<li class="add-on"> <i class="bi bi-plus-square-fill"></i>
+                                                    ${inc_value} ${inclusion} 
+                                                </li>`;
+                                            }else if(included === ''){
+                                                pr_str +=
+                                                `<li> <i class="bi bi-x-square-fill"></i>
+                                                    ${inc_value} ${inclusion}
+                                                </li>`;
+                                            }else{
+                                                pr_str +=
+                                                `<li> <i class="bi bi-check-square"></i>
+                                                    ${inc_value} ${inclusion}
+                                                </li>`;
+                                            }
                                     
-//                                         }
-//                                     });
-//                                     pr_str += `</ul>`
-//                                 }
-//                             });
-//                         }
-//                         pr_str += 
-//                         `<button class="btn rounded-0 text-white bg-1 w-100 p-2 fw-bold text-uppercase" data-bs-toggle="modal" data-bs-target="#exampleModal">
-//                             Get Started
-//                         </button>
-//                     </div>
-//                 </div>
-//             </div>`;
+                                        }
+                                    });
+                                    pr_str += `</ul>`
+                                }
+                            });
+                        }
+                        pr_str += 
+                        `<button class="btn rounded-0 text-white bg-1 w-100 p-2 fw-bold text-uppercase" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                            Get Started
+                        </button>
+                    </div>
+                </div>
+            </div>`;
 
-//             document.getElementById("table_pr").innerHTML += pr_str;
-//         });
+            document.getElementById("table_pr").innerHTML += pr_str;
+        });
 
-//     }
+    }
 
-// }).catch(error => {
-//     console.log(error);
-// });
+}).catch(error => {
+    console.log(error);
+});
 
 // Initialize Swiper
 const swiper = new Swiper('.swiper', {
